ea0cb20e3e1b59630eac497e4d9080563258eb88
efefa555 <111449616+efefa555@users.noreply.github.com>
2025-02-12 23:32:55 +0100

    Delete SOLCIV1 directory

--- SOLCIV1/backend/app/schemas.py
+++
@@ -1,7 +0,0 @@
-from app import ma
-from app.models import Building
-
-class BuildingSchema(ma.SQLAlchemyAutoSchema):
-    class Meta:
-        model = Building
-        load_instance = True
--- SOLCIV1/backend/app/utils.py
+++
@@ -1,8 +0,0 @@
-# Utility functions
-def calculate_solar_gain(orientation, windows):
-    # Placeholder calculation
-    return 200
-
-def calculate_thermal_loss(materials, insulation):
-    # Placeholder calculation
-    return 300
